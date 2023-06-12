85811526c0379e8096348e47206990220d82e805
michellemwangi01 <michellemwangim@gmail.com>
2023-06-12 15:38:29 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// ***********************
+let num1 = 31, num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+
+// ***********************
+let random = Math.floor(Math.random()*10);
+Number.isInteger(random);
+
+
+
+// ***********************
+let num3 = 24, num4 = 5
+let mod = num3 % num4;
+
+// ***********************
+let max = Math.max(20, 12, 14, 17);
